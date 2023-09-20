b6882eeaeb992e7a7b9d1fcb3fadfcdf30da8b8e
Power Mostafa <powermostafa50015@gmail.com>
2023-09-20 21:07:40 +0300

    add data validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,11 +8,33 @@ module.exports = {
     try {
       const user = await User.create({ name, email, password, image });
       res.status(201).json(user);
-    } catch (error) {
-      res.status(400).json(error);
+    } catch (err) {
+      const errors = handleErrors(err);
+      res.status(400).json({ errors });
     }
   },
   login(req, res) {
     console.log("hello login");
   },
 };
+
+
+
+function handleErrors(err) {
+  let errors = { email: "", password: "" };
+
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+  
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+}
--- models/User.js
+++ models/User.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Please enter user name']
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter an email'],
     unique: true,
     lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 8,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   },
   image: {
     type: String,
